5d707a512093900fb9b004aab3bc14377538b005
luisgrange <luisfernando237grange@outlook.com>
2022-02-27 15:32:00 -0300

    create page

---
+++ home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel Website</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />   <!-- swiper css link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">   <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="css/style.css">    <!-- custom css file link  -->
+</head>
+<body> 
+
+    <!-- header section -->
+    <section class="header">
+        <a href="home.php" class="logo">travel.</a>
+
+        <nav class="navbar">
+            <a href="home.php"></a>
+            <a href="about.php"></a>
+            <a href="package.php"></a>
+            <a href="book.php"></a>
+        </nav>
+    </section>
+    <!-- header section - end -->
+
+</body>
+</html>
